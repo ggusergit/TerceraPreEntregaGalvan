7ca78d8ccca5d86e5e1dadf36428e6d8a192e21b
gogal <gabriel.o.galvan@gmail.com>
2023-07-14 00:12:52 -0300

    Punto seguro

--- AppVet/models.py
+++ AppVet/models.py
@@ -8,11 +8,17 @@ class Veterinario(models.Model):
     matricula = models.IntegerField()
     especialidad = models.CharField(max_length=15)
 
+    def __str__(self):
+        return f'Nombre: {self.nombre} Apellido: {self.apellido} Matricula: {self.matricula} Especialidad: {self.especialidad}'
+
 class Mascota(models.Model):
     nombre = models.CharField(max_length=20)
     edad = models.IntegerField()
     tipo = models.CharField(max_length=12)
     sexo = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'Nombre: {self.nombre} Edad: {self.edad} Tipo: {self.tipo} Sexo: {self.sexo}'
 
 class Dueño(models.Model):
     nombre = models.CharField(max_length=30)
@@ -21,6 +27,12 @@ class Dueño(models.Model):
     telefono = models.IntegerField()
     direccion = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f'Nombre: {self.nombre} Apellido: {self.apellido} Edad: {self.edad} Telefono: {self.telefono} Direccion: {self.direccion}'
+
 class Turno(models.Model):
     fecha = models.DateField()
     hora = models.IntegerField()
+
+    def __str__(self):
+        return f'Fecha: {self.fecha} Hora: {self.hora}'
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Superuser: admin 
+clave: admin123admin
+
+Para probar pueden buscar con los siguientes datos por clase: 
+Veterinario: buscar apellido Gomez 
+Mascota: buscar nombre Clifford o Wilson 
+Dueño: buscar apellido Galvan o Lopez 
+Turno buscar por fecha 4/07/2023
+
+También pueden generar un nuevo elemneto de cada clase y buscarlo
